91a048c1918be22a0f881caf5c036449c0c35c6c
DENGYEWU <dengyewu@DENGYEWUdeMacBook-Pro.local>
2017-02-24 10:58:26 +0800

    Notification案例

---
+++ NotificationDemo/NotificationDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NotificationDemo
+//
+//  Created by DENGYEWU on 17/2/24.
+//  Copyright © 2017年 DENGYEWU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
